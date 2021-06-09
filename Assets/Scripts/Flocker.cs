d34a4464f05d4227ac12aa81a5cb644c55376690
johnasharifi <johnasharifi@gmail.com>
2021-06-08 22:22:47 -0700

    Add code for boid flocking
    Implement O(N * N) flocking with a static hashset

--- Assets/Scripts/Flocker.cs
+++ Assets/Scripts/Flocker.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Flocker : MonoBehaviour
 {
+	// set once at static time
+	private static readonly HashSet<Flocker> flock = new HashSet<Flocker>();
+
+	private float flockDistanceThreshold = 10.0f;
+	private int flockCountThreshold = 10;
+
+	private float speed = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+		// randomize the flocker's speeds so we can see differentitation
+		speed = speed * Random.Range(0.9f, 1.1f);
+		flock.Add(this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+		transform.forward = Vector3.RotateTowards(transform.forward, GetForceVector(), 2f * Time.deltaTime, 2f * Time.deltaTime);
+		transform.position += transform.forward * Time.deltaTime * speed;
     }
+
+	Vector3 GetForceVector() {
+		Vector3 total = new Vector3();
+
+		int adjacentCount = 0;
+
+		foreach (Flocker flocker in flock) {
+			Vector3 diff = flocker.transform.position - transform.position;
+			if (diff.sqrMagnitude < flockDistanceThreshold * flockDistanceThreshold)
+				adjacentCount++;
+			total += (flocker.transform.position - transform.position);
+		}
+
+		total /= flock.Count;
+		// when too few flock friends within range, move toward them. else move away
+		total = total * (adjacentCount > flockCountThreshold ? -1 : 1);
+		// we do not want the force vector to have a scale factor
+		total.Normalize();
+		return total;
+	}
 }
